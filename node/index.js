7a30ce90b596cd5387ef552a570e35f91da4d77d
lucas <lucas.ferreira@psicologiaviva.com.br>
2023-08-19 16:12:35 -0300

    commit inicial

---
+++ node/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connectionInsert = mysql.createConnection(config)
+
+const insert = `INSERT INTO people(name) values('Usuario1');`
+connectionInsert.query(insert)
+connectionInsert.end()
+
+app.get('/', (req, res) => {
+
+    const connection = mysql.createConnection(config)
+    const sql = `select * from  people;`
+
+    connection.query(
+        sql,
+        function select(error, results, fields) {
+            if(error) {
+                console.log(error);
+                connection.end();
+                              
+            }
+            if(results.length > 0) {
+                //console.log(results);
+                results = JSON.stringify(results)
+                res.send("<h1>Full Cycle Rocks!!</h1> </br></br>"+ results)
+            } else {
+                console.log('No data');
+            }
+            connection.end();
+        });
+})
+
+app.listen(port, ()=> {
+    console.log("Rodando" + port)
+})
